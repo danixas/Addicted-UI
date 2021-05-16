c35846b1b4c43b7da4a9b00e835dba5b218082ea
danielius <danielius.mozuras1@gmail.com>
2021-05-16 04:25:31 +0300

    bet coin distribution, pretty broken ids dont match need fixing

--- src/api/bets.js
+++ src/api/bets.js
@@ -10,10 +10,13 @@ const deleteBet = async(id) => axios.delete(`/bets/${id}`);
 
 const deleteBetOption = async(betId, optionId) => axios.delete(`/bets/${betId}/options/${optionId}`);
 
+const finishBet = async(bet_id, option_id) => axios.post(`/bets/${bet_id}/finish/${option_id}`);
+
 export {
     getAllBets,
     addNewBet,
     updateBet,
     deleteBet,
     deleteBetOption,
+    finishBet,
 };
